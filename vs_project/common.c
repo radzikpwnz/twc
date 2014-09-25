cbac13387ff7326f5b4d2b254caca330fe9706a8
RADZIK <radzikpwnz@gmail.com>
2014-09-25 21:09:34 +0400

    main.c and common.c style

--- vs_project/common.c
+++ vs_project/common.c
@@ -4,10 +4,15 @@
 
 #include "interface.h"
 
+/* Program version */
 TCHAR program_version_str[] = T("1.0 beta 5");
+
 
 #undef free
 
+/**
+ * Frees pointer and writes NULL to it
+ */
 void my_free(void **p)
 {
 	if (*p) free(*p);
@@ -16,47 +21,78 @@ void my_free(void **p)
 
 #define free(ptr) my_free(&(ptr))
 
-int SetString(TCHAR **str, TCHAR *new_str, int copy)
-{
-	if (*str != new_str) free(*str);
 
-	if (new_str == NULL) {
+/**
+ * Set one string to another.
+ *
+ * If <copy> is TRUE, copies new_str to str.
+ */
+int SetString( TCHAR **str, TCHAR *new_str, int copy)
+{
+	if ( *str != new_str ) free(*str);
+
+	if ( new_str == NULL ) {
 		*str = NULL;
 		return 1;
 	}
 
-	if (copy) {
-		*str = malloc((_tcslen(new_str) + 1) * sizeof(TCHAR));
-		_tcscpy(*str, new_str);
+	if ( copy ) {
+		*str = malloc( (_tcslen(new_str) + 1) * sizeof(TCHAR));
+		_tcscpy( *str, new_str);
 	} else {
 		*str = new_str;
 	}
 	return 1;
 }
 
-void SetStatusText(TCHAR *str)
+/**
+ * Set status text.
+ */
+void SetStatusText( TCHAR *str)
 {
-	SendMessage(StatusBar.hwnd, SB_SETTEXT, 0, (LPARAM)str);
+	SendMessage( StatusBar.hwnd, SB_SETTEXT, 0, (LPARAM)str);
 	return;
 }
 
-TCHAR *_mytcscpy(TCHAR *dest, const TCHAR *source)
+/**
+ * Copy string and return pointer to destination's end (terminating 0).
+ */
+TCHAR *_mytcscpy( TCHAR *dest, const TCHAR *source)
 {
-	while(*dest++ = *source++);
+	while( *dest++ = *source++ );
 	return dest - 1;
 }
 
+/**
+ * Get program version string.
+ */
 const TCHAR *GetProgramVersion()
 {
     return program_version_str;
 }
 
+/**
+ * Terminate program and show error message.
+ */
 void twc_Fatal( TCHAR *file, int line)
 {
     TCHAR buf[256];
 
     _stprintf( buf, T("Assertion failed in file \"%s\", line %d. Program will be terminated!"), file, line);
     MessageBox( hMainWnd, buf, T("Error!"), 0);
-    ExitProcess(1);
+    ExitProcess( 1);
     return;
 }
+
+/**
+ * Add text to log.
+ */
+/*void AddLog(const TCHAR *text)
+{
+	register int n;
+
+	n = GetWindowTextLength(txtLog.hwnd);
+	SendMessage(txtLog.hwnd, EM_SETSEL, (WPARAM)n, (LPARAM)n);
+	SendMessage(txtLog.hwnd, EM_REPLACESEL, 0, (LPARAM) text);
+	return;
+}*/
--- vs_project/main.c
+++ vs_project/main.c
@@ -21,97 +21,132 @@ processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
 #include "mainwnd.h"
 #include "window.h"
 
-//#define VLD_FORCE_ENABLE
-//#include "vld.h"
+/*
+#define VLD_FORCE_ENABLE
+#include "vld.h"
+*/
 
-//void AddLog(const TCHAR *text);
+/**
+ * Global variables
+ */
+int new_control = 0;       /* Flag indicates that new control is being created from toolbox */
+int grid_size = 2;         /* Grid size */
+RT_OBJECT *current_object; /* Pointer to current object info */
+HWND hMainWnd;             /* Main window handle */
+HWND hPropList;            /* Property list handle */
 
-//Windows params
-HWND hMainWnd;
-WNDCLASSEX wincl = {sizeof (WNDCLASSEX), /*CS_HREDRAW | CS_VREDRAW*/0, MainWndProc, 0, 0, 0, 0, 0, (HBRUSH)(COLOR_BTNFACE + 1), MAKEINTRESOURCE(IDR_MENU1), T("MainWnd"), 0};
-
-int new_control = 0;
-int grid_size = 2;
-
-RT_OBJECT *current_object;
-
-HWND hPropList;
-
-HMODULE msft_mod;
-
-int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
-//int _WinMain()
+/**
+ * Creates property list.
+ */
+static int CreatePropList()
 {
-	MSG msg;
-	PL_ITEMINFO iteminfo;
-	/*RECT rect;
-	HWND hTmp;*/
+    TWC_CHECKIT( hMainWnd != NULL );
 
-	LoadControlCursors();
+    hPropList = CreateWindowEx( 0, GetPropListClassname(), NULL,
+                                WS_BORDER | WS_VISIBLE | WS_CHILD | WS_CLIPSIBLINGS | WS_CLIPCHILDREN | WS_VSCROLL,
+                                0, 0, 150, 0, hMainWnd, NULL, GetModuleHandle( NULL), NULL);
+    if ( hPropList == NULL ) {
+        return 0;
+    }
 
-	//Main Window
-	wincl.hCursor = LoadCursor(NULL, IDC_ARROW);
-	if (!RegisterClassEx (&wincl)) return 0;
+    /* Setting z-order(top), item height and font */
+	SetWindowPos( hPropList, HWND_TOP, 0, 0, 0, 0, SWP_NOMOVE | SWP_NOSIZE);
+	SendMessage( hPropList, PL_SETITEMHEIGHT, 20, 0);
+	SendMessage( hPropList, WM_SETFONT, (WPARAM)GetStockObject( DEFAULT_GUI_FONT), 0);
+}
 
-	//Default Window
+/**
+ * Registers window classes
+ */
+static int RegisterWindowClasses()
+{
+    static WNDCLASSEX wincl = { sizeof (WNDCLASSEX), 0, MainWndProc, 0, 0, 0, 0, 0,
+                                (HBRUSH)(COLOR_BTNFACE + 1), MAKEINTRESOURCE(IDR_MENU1), T("MainWnd"), 0 };
+
+    /* Main Window */
+	wincl.hCursor = LoadCursor( NULL, IDC_ARROW);
+	if ( !RegisterClassEx( &wincl) ) return 0;
+
+	/* Default Window */
 	wincl.style = CS_DBLCLKS;
 	wincl.lpszMenuName = NULL;
 	wincl.lpszClassName = GetControlClassname( CTRL_ID_WINDOW);
 	wincl.lpfnWndProc = DefWindowProc;
-	if (!RegisterClassEx (&wincl)) return 0;
+	if ( !RegisterClassEx( &wincl) ) return 0;
 
-	//Code window
+	/* Code window */
 	wincl.style = 0;
 	wincl.lpszMenuName = NULL;
 	wincl.lpszClassName = T("CodeWnd");
 	wincl.lpfnWndProc = CodeWndProc;
-	if (!RegisterClassEx (&wincl)) return 0;
+	if ( !RegisterClassEx( &wincl) ) return 0;
 
-	InitCommonControlsEx(NULL);
-	if (InitPropertyListDll(GetModuleHandle(NULL)) == 0) return 0;
-
-	if (!twcInit() || !(twcCreateObject(&MainWnd, 1, HWND_DESKTOP))) return 0;
-	hMainWnd = MainWnd.hwnd;
-
-	hPropList = CreateWindowEx(0, GetPropListClassname(), NULL, WS_BORDER | WS_VISIBLE | WS_CHILD | WS_CLIPSIBLINGS | WS_CLIPCHILDREN | WS_VSCROLL, 0, 0, 150, 0, hMainWnd, NULL, GetModuleHandle(NULL), NULL);
-	SetWindowPos(hPropList, HWND_TOP, 0, 0, 0, 0, SWP_NOMOVE | SWP_NOSIZE);
-
-	SetWindowPos(StatusBar.hwnd, HWND_TOP, 0, 0, 0, 0, SWP_NOMOVE | SWP_NOSIZE);
-
-	iteminfo.id = 0;
-	/*hTmp = CreateWindowEx(0, WC_COMBOBOX, NULL, WS_CHILD | CBS_DROPDOWN, 0, 0, 0, 0, hMainWnd, NULL, GetModuleHandle(NULL), NULL);
-	GetClientRect(hTmp, &rect);
-	DestroyWindow(hTmp);*/
-
-	SendMessage(hPropList, PL_SETITEMHEIGHT, 20, 0);
-	SendMessage(hPropList, WM_SETFONT, (WPARAM)GetStockObject(DEFAULT_GUI_FONT), 0);
-
-	AddToolboxButtons(Toolbox.hwnd);
-	AddMainToolbarButtons(MainToolbar.hwnd);
-
-	ShowWindow(hMainWnd, SW_SHOWNORMAL);
-	UpdateWindow(hMainWnd);
-	SetStatusText(T("Ready!"));
-
-	msft_mod = LoadLibrary(T("Msftedit.dll"));
-	LoadLibrary(T("Riched20.dll"));
-	if (msft_mod == NULL) {
-		MessageBox(hMainWnd, T("Unable to load Msftedit.dll, some code edit functions will be disabled!"), T("Warning!"), 0);
-	}
-
-	while (GetMessage (&msg, NULL, 0, 0)) {
-		TranslateMessage(&msg);
-		DispatchMessage(&msg);
-	}
-	ExitProcess(msg.wParam);
+    return 1;
 }
 
-/*void AddLog(const TCHAR *text)
+/**
+ * Program initialization
+ */
+static int Init()
 {
-	register int n;
-	
-	n = GetWindowTextLength(txtLog.hwnd);
-	SendMessage(txtLog.hwnd, EM_SETSEL, (WPARAM)n, (LPARAM)n);
-	SendMessage(txtLog.hwnd, EM_REPLACESEL, 0, (LPARAM) text); 
-	return;
-}*/
+    /* Loading control cursors */
+	LoadControlCursors();
+
+    /* Registering window classes */
+    if ( RegisterWindowClasses() == 0 )
+        return 0;
+    }
+
+    /* Initializing common controls library */
+    InitCommonControlsEx(NULL);
+
+    /* Initializing property list dll */
+	if ( InitPropertyListDll( GetModuleHandle(NULL)) == 0 ) {
+        return 0;
+    }
+
+    /* Initializing TWC and creating main window */
+    if ( !twcInit() || !(twcCreateObject( &MainWnd, 1, HWND_DESKTOP)) ) {
+        return 0;
+    }
+	hMainWnd = MainWnd.hwnd;
+
+    /* Creating property list */
+    if ( CreatePropList() == 0) {
+        return 0;
+    }
+
+    /* Setting status bar z-order(top) */
+    SetWindowPos( StatusBar.hwnd, HWND_TOP, 0, 0, 0, 0, SWP_NOMOVE | SWP_NOSIZE);
+
+    /* Adding toolbox and toolbar buttons */
+    AddToolboxButtons(Toolbox.hwnd);
+	AddMainToolbarButtons(MainToolbar.hwnd);
+
+    /* Show main window */
+    ShowWindow( hMainWnd, SW_SHOWNORMAL);
+	UpdateWindow( hMainWnd);
+	SetStatusText( T("Ready!"));
+
+    return 1;
+}
+
+/**
+ * Program entry point.
+ */
+int CALLBACK WinMain( HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+	MSG msg;
+
+    /* Initialization */
+    if ( Init() == 0 ) {
+        MessageBox( HWND_DESKTOP, T("Error during initialization!", T("Error"), 0),
+    }
+
+    /* Main loop */
+	while ( GetMessage( &msg, NULL, 0, 0)) {
+		TranslateMessage( &msg);
+		DispatchMessage( &msg);
+	}
+	ExitProcess( msg.wParam);
+}
